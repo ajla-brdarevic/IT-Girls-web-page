c2e66ebe3f0e11be891b2cfac85b4f1ac3ea81a0
ajla00 <93090001+ajla00@users.noreply.github.com>
2021-12-27 15:38:55 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+//DARK AND LIGHT MODE
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+});
+
+window.onscroll = function() {scrollFunction()};
+
+
